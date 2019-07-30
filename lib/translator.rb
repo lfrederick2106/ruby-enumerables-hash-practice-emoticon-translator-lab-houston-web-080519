621794463d86e5e5ad2b169f95fe2fc55511817f
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-07-30 17:39:38 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,26 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
-end
+ def load_library(emoticon_file)
+   load_library = YAML.load_file("./lib/emoticons.yml")
+   
+   "get_meaning" = Hash.new 
+   get_meaning[n[1]] = k 
+ 
+ end
 
-def get_japanese_emoticon
-  # code goes here
-end
+# def get_japanese_emoticon(western_emoticon)
+#   # code goes here
+#   load_library(emoticon_file)
+#   load_library.select{|key, hash| hash["client_id"] == "2180" }
+# end
 
-def get_english_meaning
-  # code goes here
-end
+# # def get_english_meaning
+# #   # code goes here
+# # end
+
+
+#load_library = YAML.load_file("./lib/emoticons.yml")
+
+#p load_library["angel"][0]
+
+p get_meaning[o_O]
